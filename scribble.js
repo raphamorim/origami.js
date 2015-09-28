3bbad0f04acc176203d0c14207d040cc016e0416
Raphael Amorim <rapha850@gmail.com>
2015-09-27 23:15:54 -0300

    first sign of scribble

--- scribble.js
+++ scribble.js
@@ -1,5 +1,5 @@
 var scribble = (function(){
-   this.elements = [];
+   this.contexts = [];
    this.on = function(){
        [].slice.call(arguments);
        console.log(arguments);
@@ -8,12 +8,45 @@ var scribble = (function(){
           this.createCanvasContext(arguments[i]);
        }
 
-       console.log(this.elements);
+       return this;
    }
 
    this.createCanvasContext = function(el) {
-       var canvas = document.querySelector(el);
-       this.elements.push((canvas.getContext('2d') || false));
+       var canvas = document.querySelector(el),
+           context = canvas.getContext('2d');
+
+       this.contexts.push((context || false));
+   }
+
+   //#TODO: Remove this hack
+   this.args = function(argsArray){
+       var argsPattern = ['x', 'y', 'width', 'height'],
+           args = new Object();
+       for (var i = 0; i < argsArray.length; i++) {
+          if (!argsPattern.length)
+            break;
+          if (typeof(argsArray[i]) === "object")
+            args["style"] = argsArray[i];
+          else {
+            args[argsPattern[0]] = argsArray[i];
+            argsPattern = argsPattern.slice(1);
+          }
+       }
+
+       return args;
+   }
+
+   this.square = function(x, y, size){
+       var args = this.args(([].slice.call(arguments) || []));
+       console.log(args);
+       for (var i = 0; i < contexts.length; i++){
+          contexts[i].beginPath();
+          contexts[i].lineWidth="4";
+          contexts[i].strokeStyle="green";
+          contexts[i].rect(args.x, args.y, args.width, args.width);
+          contexts[i].stroke();
+       }
+       return this;
    }
 
    return {
