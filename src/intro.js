b29e65f2ce22417d07ed87ff52313914e6fdd79b
Raphael Amorim <rapha850@gmail.com>
2016-01-14 05:52:39 -0200

    create src base

---
+++ src/intro.js
@@ -0,0 +1,11 @@
+/*!
+ * Origami.js @VERSION
+ * https://origamijs.com/
+ *
+ * Copyright Raphael Amorim 2016
+ * Released under the GPL-4.0 license
+ *
+ * Date: @DATE
+ */
+
+(function( global ) {
