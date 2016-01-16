5ca940384c037cfccd53e9464772cda1b78d10eb
Raphael Amorim <rapha850@gmail.com>
2016-01-15 22:44:11 -0200

    add process in Gruntfile, update comment, redefine line strokeStyle def

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,20 @@
 module.exports = function(grunt) {
+    function process( code, filepath ) {
+
+       return code
+
+            // Embed version
+            .replace( /@VERSION/g, grunt.config( "pkg" ).version )
+
+            // Embed date (yyyy-mm-ddThh:mmZ)
+            .replace( /@DATE/g, ( new Date() ).toISOString().replace( /:\d+\.\d+Z$/, "Z" ) );
+    }
+
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         concat: {
             "src-js": {
+                options: { process: process },
                 src: [
                     "src/intro.js",
                     "src/core/config.js",
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
-
-// Missing Description
+// Check if element exists in a Array of NodeItems
 function exists(el, arr) {
     for (var i = 0; i < arr.length; i++) {
         if (arr[i].element.isEqualNode(el))
