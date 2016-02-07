d77be7dda0354926c2165ce7f4186379198a74a8
Raphael Amorim <rapha850@gmail.com>
2016-02-07 16:29:11 -0200

    added tests to image draw method

--- test/test-draw.js
+++ test/test-draw.js
@@ -1,4 +1,4 @@
-describe("Draw", function() {
+describe("Draw method", function() {
 
   // Arc Draw
   context('arc', function() {
@@ -25,7 +25,7 @@ describe("Draw", function() {
     context('with blue background', function() {
       it("should create a circle with a blue background", function(done) {
         var canvas1 = document.querySelector('#canvas1'),
-            canvas2 = document.querySelector('#canvas2');
+          canvas2 = document.querySelector('#canvas2');
 
         // Drawing
         this.timeout(5000);
@@ -46,6 +46,60 @@ describe("Draw", function() {
         ctx2.strokeStyle = 'gold';
         ctx2.stroke();
         ctx2.closePath();
+
+        setTimeout(function() {
+          var isEqual = imagediff.equal(canvas1, canvas2);
+          expect(isEqual).to.eql(true);
+
+          done();
+        }, 1500);
+      });
+    });
+  });
+
+  // Image Draw
+  context('image', function() {
+    beforeEach(function() {
+      var canvas = document.createElement("canvas");
+      canvas.id = 'canvas1';
+      canvas.width = 500;
+      canvas.height = 500;
+      document.body.appendChild(canvas);
+
+      var canvasMock = document.createElement("canvas");
+      canvasMock.id = 'canvas2';
+      canvasMock.width = 500;
+      canvasMock.height = 500;
+      document.body.appendChild(canvasMock);
+    });
+
+    afterEach(function() {
+      document.body.removeChild(document.querySelector('#canvas1'));
+      document.body.removeChild(document.querySelector('#canvas2'));
+      origami.cleanContexts();
+    })
+
+    context('with a valid image', function() {
+      it("should create a canvas with setted image", function(done) {
+        var canvas1 = document.querySelector('#canvas1'),
+          canvas2 = document.querySelector('#canvas2');
+        var imageSource = '../images/firefox.png';
+
+        origami('#canvas1')
+          .image(imageSource, 10, 10)
+          .load(function(canvas) {
+            canvas.draw();
+          });
+
+        var ctx2 = canvas2.getContext('2d');
+        var img = new Image();
+        img.src = imageSource;
+
+        img.addEventListener('load', function() {
+          ctx2.beginPath();
+          ctx2.drawImage(img, 10, 10, img.naturalWidth, img.naturalHeight);
+          ctx2.closePath();
+        })
 
         setTimeout(function() {
           var isEqual = imagediff.equal(canvas1, canvas2);
