a5efde5760daa43e7534ba6568cf86ef868abaf3
Guilherme Paixão <guilhermee.paixaoo@gmail.com>
2016-10-28 11:07:29 -0200

    Include format required by Codacy

--- test/test-resources.js
+++ test/test-resources.js
@@ -11,14 +11,14 @@ CanvasRenderingContext2D.prototype.setLineDash = function(dash) {
 // Mocked only for test
 
 var lastTime = 0;
-var vendors = ['ms', 'moz', 'webkit', 'o'];
+var vendors = ["ms", "moz", "webkit", "o"];
 for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-  window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-  window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-  || window[vendors[x]+'CancelRequestAnimationFrame'];
+  window.requestAnimationFrame = window[vendors[x]+"RequestAnimationFrame"];
+  window.cancelAnimationFrame = window[vendors[x]+"CancelAnimationFrame"]
+  || window[vendors[x]+"CancelRequestAnimationFrame"];
 }
 
-if (!window.requestAnimationFrame)
+if (!window.requestAnimationFrame) {
   window.requestAnimationFrame = function(callback, element) {
     var currTime = new Date().getTime();
     var timeToCall = Math.max(0, 16 - (currTime - lastTime));
@@ -26,11 +26,13 @@ if (!window.requestAnimationFrame)
     lastTime = currTime + timeToCall;
     return id;
   };
+}
 
-if (!window.cancelAnimationFrame)
+if (!window.cancelAnimationFrame) {
   window.cancelAnimationFrame = function(id) {
     clearTimeout(id);
   };
+}
 
 
 
